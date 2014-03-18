b222b00e3c85c45f26a5ad8ec05b58de1698c605
kimi <kimi521@gmail.com>
2014-03-19 00:16:13 +0800

    forum configurations

---
+++ src/main/java/com/kimi/forum/web/LoginController.java
@@ -0,0 +1,21 @@
+package com.kimi.forum.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	/**
+	 * 登录注销
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/doLogout")
+	public String logout() {
+		System.out.println("logout....................");
+		return "forward:/index.jsp";
+	}
+}
